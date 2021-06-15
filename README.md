cc2df46b94f85dca0066faf0509cae9af19460b1
何梓杰 <hezijie@xst008.cn>
2021-06-15 13:05:58 +0800

    第一版本提交

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # APISIX
 
+
+### 服务发现
+需要把脚本挂载到容器内部的```/usr/local/apisix/apisix/discovery/```路径下
+如```./discovery/consul.lua:/usr/local/apisix/apisix/discovery/consul.lua```
+
+修改配置
+`config/apisix_config.yam`
+
+```yml
+discovery:
+  consul:
+    servers:
+      - "http://consul-node1:8500"
+      - "http://consul-node2:8500"
+      - "http://consul-node3:8500"
+      - "http://consul-node4:8500"
+    dc: "dc1"
+    alc_token: "xxx"
+```
+其中discovery下的子节点命名，必须和脚本同名，如consul.lua，其节点必须为其中discovery下的子节点命名，必须和脚本同名，如consul
+consule节点下，servers是必填项，dc和alc_token为选项
+
+之后在每个路由配置为使用服务发现配置`upstream`字段
+```json
+  "uris":["/test/*"],
+  "name":"test",
+  "methods":["GET"],
+  "plugins": {
+    "proxy-rewrite": {
+      "regex_uri": [
+        "^/test/(.*)",
+        "/${1}"
+      ]
+    }
+  },
+  "upstream": {
+    "type": "roundrobin",
+    "discovery_type": "consul",
+    "service_name": "TestService"
+  },
+```
+
+
---
+++ discovery/consul.lua
@@ -0,0 +1,133 @@
+local require            = require
+local local_conf         = require("apisix.core.config_local").local_conf()
+local core               = require("apisix.core")
+local cjson              = require('cjson')
+local http               = require('resty.http')
+local log                = core.log
+local ipairs             = ipairs
+local math               = math
+local random             = math.random
+
+local consul_conf = local_conf.discovery.consul
+
+local schema = {
+    type = "object",
+    properties = {
+        servers = {
+            type = "array",
+            minItems = 1,
+            items = {
+                type = "string"
+            }
+        },
+        dc = {
+            type = "string",
+            minItems = 1
+        },
+        alc_token = {
+            type = "string",
+            minItems = 1
+        }
+    },
+    required = {"servers"}
+}
+
+
+local _M = {
+    version = 0.1,
+}
+
+
+local function get_consul_server_endpoint(service_name)
+    local server = consul_conf.servers[random(1, #consul_conf.servers)]
+    if string.sub(server,#server) == "/" then
+        server = string.sub(server, 1, #server - 1)
+    end
+    
+    local path = "/v1/catalog/service/" .. service_name
+
+    local split = "?"
+
+    if consul_conf.dc ~= nil and consul_conf.dc ~= "" then
+        path = path .. split .. "dc=" .. consul_conf.dc
+        if split == "?" then
+            split = "&"
+        end
+    end
+
+    if consul_conf.alc_token ~= nil and #consul_conf.aalc_tokenlc ~= "" then
+        path = path .. split .. "token=" .. consul_conf.alc_token
+        if split == "?" then
+            split = "&"
+        end
+    end
+
+    local endpoint = server .. path
+    
+    return endpoint
+end
+
+
+local function get_nodes(service_name)
+    -- 获取 consul 服务器节点
+    local endpoint = get_consul_server_endpoint(service_name)
+
+    local http_client = http.new()
+
+    -- 向consul服务器发起http请求，获取服务节点
+    local response, error = http_client:request_uri(endpoint, {
+        method = "GET"
+    })
+
+    if not response then
+        log.error("request error: ", error)
+        return
+    end
+
+    -- 反序列化
+    local json_convert = cjson.new()
+    local response_body = json_convert.decode(response.body)
+
+    if not response_body or #response_body == 0 then
+        log.error("cannot get service ["..service_name.."] nodes, please check the service status in consul")
+        return
+    end
+
+    local nodes = {}
+
+    -- 转化为apisix需要的格式
+    for index, node in ipairs(response_body) do
+        nodes[index] = {
+            host = node.ServiceAddress,
+            port = node.ServicePort,
+            weight = 100,
+            metadata = {
+                management = {
+                    port = node.ServicePort
+                }
+            }
+        }
+    end
+
+    return nodes
+end
+
+-- 获取服务节点列表
+function _M.nodes(service_name)
+    local nodes = get_nodes(service_name)
+    return nodes
+end
+
+-- 初始化
+function _M.init_worker()
+    -- 检查是否有配置
+    local ok, err = core.schema.check(schema, consul_conf)
+    if not ok
+        then
+            log.error("配置错误：" .. err)
+            return
+        end
+end
+
+
+return _M
