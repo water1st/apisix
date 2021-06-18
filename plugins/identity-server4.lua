78e66417ddbab92e906905231f115c2f392b30e3
何梓杰 <hezijie@xst008.cn>
2021-06-18 17:38:36 +0800

    验证token通过

--- plugins/identity-server4.lua
+++ plugins/identity-server4.lua
@@ -11,12 +11,6 @@ local plugin_name = "identity-server4"
 
 local schema = {
     type = "object",
-    additionalProperties = false,
-    properties = {},
-}
-
-local consumer_schema = {
-    type = "object",
     properties = {
         identity_server_uri = {
             type = "string",
@@ -56,12 +50,11 @@ end
 
 local function get_jwt_token(context)
     local token = core.request.header(context, "authorization")
-
     if token ~= nil and token ~= "" then
         
         local token_infos = split(token," ")
-        local token_prefix = string.lower(token_infos[0])
-        local jwt_token = token_infos[1]
+        local token_prefix = string.lower(token_infos[1])
+        local jwt_token = token_infos[2]
 
         if token_prefix == "bearer" and jwt_token ~= nil and jwt_token ~= "" then
             return jwt_token
@@ -76,22 +69,17 @@ local _M = {
     priority = 2599,
     type = 'auth',
     name = plugin_name,
-    schema = schema,
-    consumer_schema = consumer_schema
+    schema = schema
 }
 
 function _M.rewrite(config, context)
-    logger.warn("config：" .. core.json.encode(config, true))
-    logger.warn("context：" .. core.json.encode(context, true))
-
     local jwt_token = get_jwt_token(context)
     if not jwt_token then
-        logger.warn("token not found in header")
+        logger.info("token not found in header")
         return 401, { message = "Missing JWT token in request"}
     end
 
     local jwt_object = jwt:load_jwt(jwt_token)
-    logger.warn("jwt object: ", core.json.delay_encode(jwt_object))
     if not jwt_object.valid then
         return 401, { message = jwt_object.reason }
     end
@@ -100,7 +88,6 @@ function _M.rewrite(config, context)
     if not verified then
         return 401, { message = jwt_object.reason }
     end
-
 end
 
 function _M.check_schema(conf)
